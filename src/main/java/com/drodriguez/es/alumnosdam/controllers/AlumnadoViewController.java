a5a0ba905d7338875b45f8eeec44d6a3af7869b2
Daniel Rodríguez Fernández <90853739+idanirf@users.noreply.github.com>
2022-06-19 14:03:45 +0200

    Vista

--- src/main/java/com/drodriguez/es/alumnosdam/controllers/AlumnadoViewController.java
+++ src/main/java/com/drodriguez/es/alumnosdam/controllers/AlumnadoViewController.java
@@ -1,4 +1,29 @@
 package com.drodriguez.es.alumnosdam.controllers;
 
+import javafx.event.ActionEvent;
+
 public class AlumnadoViewController {
+    public void onCerrarAplicacion(ActionEvent actionEvent) {
+    }
+
+    public void onImportarAction(ActionEvent actionEvent) {
+    }
+
+    public void onExportarAction(ActionEvent actionEvent) {
+    }
+
+    public void onPrintInformeAction(ActionEvent actionEvent) {
+    }
+
+    public void onRegistrarAlumnoAction(ActionEvent actionEvent) {
+    }
+
+    public void onEliminarAlumnoAction(ActionEvent actionEvent) {
+    }
+
+    public void onActualizarAlumnoAction(ActionEvent actionEvent) {
+    }
+
+    public void onLimpiarCamposAction(ActionEvent actionEvent) {
+    }
 }
