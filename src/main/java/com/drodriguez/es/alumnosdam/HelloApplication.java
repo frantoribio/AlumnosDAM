9cd839e72c5642025b7199f257a54dbeb3c79310
Daniel Rodríguez Fernández <90853739+idanirf@users.noreply.github.com>
2022-06-19 13:46:29 +0200

    Service Storage CSV

--- src/main/java/com/drodriguez/es/alumnosdam/HelloApplication.java
+++ src/main/java/com/drodriguez/es/alumnosdam/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("alumnado-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("AlumnosDAM");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/drodriguez/es/alumnosdam/services/CSVStorage.java
@@ -0,0 +1,62 @@
+package com.drodriguez.es.alumnosdam.services;
+
+import com.drodriguez.es.alumnosdam.managers.DataBaseManager;
+import com.drodriguez.es.alumnosdam.models.Alumno;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+
+public class CSVStorage implements ICSVStorage {
+    DataBaseManager db;
+
+    @Override
+    public boolean save(Path path, List<Alumno> item) {
+        var encabezado = "id, dni, nombreApellidos, nota, fechaNacimiento";
+        var csv =new StringBuilder(encabezado);
+        var csvLista = item.stream().map(this::toCsv).toList();
+        for (String st : csvLista) {
+            csv.append("\n");
+            csv.append(st);
+        }
+        try (var fileWriter = new FileWriter(path.toFile())){
+            fileWriter.write(csv.toString());
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error escribiendo en la base de datos");
+        }
+        return false;
+    }
+
+    @Override
+    public List<Alumno> load(Path Path) throws IOException {
+        try {
+            return Files.lines(Path).skip(1).map(this::parse).toList();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
+    }
+
+    private Alumno parse(String linea){
+        var filds = linea.split(String.valueOf(','));
+        var id = Integer.parseInt(filds[0]);
+        var dni = filds[1];
+        var nombreApellidos = filds[2];
+        var nota = Double.parseDouble(filds[3]);
+        var fechaNacimiento = LocalDate.parse(filds[4]);
+        return new Alumno(id, dni, nombreApellidos, nota, fechaNacimiento);
+    }
+
+    private String toCsv(Alumno dto){
+        return dto.getID() + ";" +
+                dto.getDni() + ";" +
+                dto.getNombreApellidos() + ";" +
+                dto.getNota() + ";" +
+                dto.getFechaNacimiento();
+    }
+}
